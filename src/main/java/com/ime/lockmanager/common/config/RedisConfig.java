7fe830ffa3b473b6704c6730f35a8a49f7fbd41f
jaepyo99 <jaepyo99@naver.com>
2023-07-20 02:56:06 +0900

    redisson config 추가

--- src/main/java/com/ime/lockmanager/common/config/RedisConfig.java
+++ src/main/java/com/ime/lockmanager/common/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.ime.lockmanager.common.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +19,20 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        RedissonClient redisson = null;
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+        redisson = Redisson.create(config);
+        return redisson;
     }
 
     @Bean
