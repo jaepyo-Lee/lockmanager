1b3f372b3a140c2416b2bd52942f75e4de4d879e
jaepyo99 <jaepyo99@naver.com>
2023-08-05 21:14:56 +0900

    시간 조회 API 시간형식 수정

--- src/main/java/com/ime/lockmanager/locker/application/port/in/res/LockerPeriodResponseDto.java
+++ src/main/java/com/ime/lockmanager/locker/application/port/in/res/LockerPeriodResponseDto.java
@@ -1,16 +1,18 @@
 package com.ime.lockmanager.locker.application.port.in.res;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 import reactor.util.annotation.Nullable;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class LockerPeriodResponseDto {
-    private LocalDateTime startDateTime=null;
+    private LocalDateTime startDateTime;
 
-    private LocalDateTime endDateTime=null;
+    private LocalDateTime endDateTime;
 
     @Builder
     public LockerPeriodResponseDto(LocalDateTime startDateTime, LocalDateTime endDateTime) {
