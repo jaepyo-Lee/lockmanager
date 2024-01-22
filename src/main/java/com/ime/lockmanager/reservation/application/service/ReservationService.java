4cdb72c2b3df2118000648b0fc94267ddbe6207c
jaepyo99 <jaepyo99@naver.com>
2024-01-23 00:05:10 +0900

    refactory: mysql필드 수정

--- src/main/java/com/ime/lockmanager/reservation/application/service/ReservationService.java
+++ src/main/java/com/ime/lockmanager/reservation/application/service/ReservationService.java
@@ -9,19 +9,15 @@ import com.ime.lockmanager.common.format.exception.user.NotFoundUserException;
 import com.ime.lockmanager.common.sse.SseEmitterService;
 import com.ime.lockmanager.locker.application.port.in.req.LockerRegisterRequestDto;
 import com.ime.lockmanager.locker.application.port.in.res.LockerRegisterResponseDto;
-import com.ime.lockmanager.locker.application.port.in.res.ReservationOfLockerResponseDto;
 import com.ime.lockmanager.locker.application.port.out.LockerDetailQueryPort;
-import com.ime.lockmanager.locker.application.port.out.LockerQueryPort;
 import com.ime.lockmanager.locker.domain.locker.Locker;
 import com.ime.lockmanager.locker.domain.lockerdetail.LockerDetail;
 import com.ime.lockmanager.reservation.application.port.in.ReservationUseCase;
 import com.ime.lockmanager.reservation.application.port.out.ReservationQueryPort;
 import com.ime.lockmanager.reservation.domain.Reservation;
-import com.ime.lockmanager.reservation.domain.ReservationStatus;
 import com.ime.lockmanager.user.application.port.in.req.UserCancelLockerRequestDto;
 import com.ime.lockmanager.user.application.port.out.UserQueryPort;
 import com.ime.lockmanager.user.domain.User;
-import com.ime.lockmanager.user.domain.UserJpaEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -29,10 +25,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static com.ime.lockmanager.common.sse.SseEmitterService.sendEvent;
-import static com.ime.lockmanager.reservation.domain.ReservationStatus.RESERVED;
 
 
 @Slf4j
@@ -79,7 +73,7 @@ public class ReservationService implements ReservationUseCase {
         Locker locker = lockerDetail.getLocker();
         distinctConditionForReserveToUser(locker);
         commonConditionForReserve(user, lockerDetail, locker);
-        Long registerLockerId = reservationQueryPort.registerLocker(UserJpaEntity.of(user), lockerDetail);
+        Long registerLockerId = reservationQueryPort.registerLocker(user, lockerDetail);
         Set<SseEmitter> sseEmitters = SseEmitterService.getEmitterMap()
                 .getOrDefault("LOCKER_" + dto.getMajorId().toString(), new HashSet<>());
         SseEmitter.SseEventBuilder reservedLockerEventBuilder = SseEmitter.event()
